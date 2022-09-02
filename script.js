952f54c698580f97777ecb6fa6fd0ccb589f1a12
biponroy47 <biponroy47@yahoo.com>
2022-09-02 02:40:16 -0400

    updated styling

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const announce = (type) => {
     switch (type) {
       case PLAYERO_WON:
-        announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+        announcer.innerHTML = 'Player <span class="playerO">O</span> Won!';
         break;
       case PLAYERX_WON:
-        announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+        announcer.innerHTML = 'Player <span class="playerX">X</span> Won!';
         break;
       case TIE:
         announcer.innerText = "Tie";
